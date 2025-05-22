2211c28a18546f4d7d269c16dbf3e0c3351aedc2
root <root@localhost>
2025-05-22 14:47:25 +0600

    auto publish 1747903645636

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
-/*Below code will show 
-"Hello World"and it is our first program*/
 #include<stdio.h>
-void main(){
-printf(" Hello World!");
+void main()
+{
+int a=5,b=4;
+int c;
+c=a+b;
+printf("The addition is %d\n",c);
+c=a-b;
+printf("The subtraction is %d\n",c);
+  c=a*b;
+  printf("The product is %d\n",c);
+  c=a/b;
+  printf("The division is %d\n",c);
+  c=a%b;
+  printf("The remainder is %d\n",c);
+  
 }
